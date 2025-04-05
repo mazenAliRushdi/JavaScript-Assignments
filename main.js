e41380e9d863dc124270e346f1db103ceaf42bda
mazen <mazen.ali.rushdy@gmail.com>
2025-04-05 20:52:18 +0200

    Cancel code execution using comments

---
+++ main.js
@@ -0,0 +1,11 @@
+// The first way to cancel the code
+
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+// The Second way to cancel the code
+
+/*
+    console.log("Iam In Console");
+    document.write("Iam In Page");
+*/
