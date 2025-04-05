437480b8f1e2f2e0f7f99896b9ec2d447ce26056
mazen <mazen.ali.rushdy@gmail.com>
2025-04-05 20:43:54 +0200

    Print a message in the console in the format

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const headingHTML =
-  '<h1 style="color: blue; font-size: 2.5em; text-align: center; background-color: #f0f0f0; padding: 15px;">هذا هو عنوان الصفحة</h1>';
-
-document.write(headingHTML);
+console.log(
+  "%cWeb%c Developer",
+  "color: red; font-weight: bold;",
+  "color: green;"
+);
