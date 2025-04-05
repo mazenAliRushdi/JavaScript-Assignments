5f82671e307cf78570e8a81c2c47873fecb72699
mazen <mazen.ali.rushdy@gmail.com>
2025-04-05 20:35:50 +0200

    Create the element in the image within the page using JavaScript

---
+++ main.js
@@ -0,0 +1,4 @@
+const headingHTML =
+  '<h1 style="color: blue; font-size: 2.5em; text-align: center; background-color: #f0f0f0; padding: 15px;">هذا هو عنوان الصفحة</h1>';
+
+document.write(headingHTML);
