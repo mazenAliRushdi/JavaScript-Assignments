207a64f48ac5577e1bf547ae44518e4fde78377e
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 08:34:53 +0200

    Generate the number 6 from variable 'num' in five different ways

---
+++ main.js
@@ -0,0 +1,16 @@
+let num = 3;
+
+// Solution One
+console.log(num + num);
+
+// Solution Two
+console.log(num * (true + true));
+
+// Soultion Three
+console.log(num + true + true + true);
+
+// Soultion Four
+console.log(num-- * num);
+
+// Solution Five
+console.log(num + Number(true) + Number(true) + Number(true) + Number(true));
