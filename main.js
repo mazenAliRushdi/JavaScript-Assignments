aef53300d56eef5b2662083852b82273e93ed8f8
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 10:33:42 +0200

    Check multiple conditions with separate if statements for specific output

---
+++ main.js
@@ -0,0 +1,19 @@
+let num1 = 10;
+let num2 = 30;
+let num3 = "30";
+
+if (num3 > num1) {
+  console.log(
+    `"${num3}" Is Larger Than ${num1} And Type ${typeof num3} Not The Same Type As ${typeof num1}`
+  );
+}
+if (num3 > num1 && num3 == num2 && typeof num3 != typeof num2) {
+  console.log(
+    `"${num3}" Is Larger Than ${num1} And Value Is The Same As ${num2} And Type ${typeof num3} Not The Same Type As ${typeof num2}`
+  );
+}
+if (num3 != num1 && typeof num3 != typeof num1 && typeof num3 != typeof num2) {
+  console.log(
+    `"${num3}" Value And Type Is Not The Same As ${num1} And Type Is Not The Same As ${num2}`
+  );
+}
