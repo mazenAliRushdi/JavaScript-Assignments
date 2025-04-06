c96d3112634e6c0148371bf3bb61bc8f49c2a082
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 09:37:48 +0200

    Extract and lowercase the first character of a string in six ways

---
+++ main.js
@@ -0,0 +1,7 @@
+let userName = "Elzero";
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.at(0).toLocaleLowerCase()); // e
+console.log(userName.slice(0, 1).toLocaleLowerCase()); // e
+console.log(userName.substring(0, 1).toLocaleLowerCase()); // e
+console.log(userName.substr(0, 1).toLocaleLowerCase()); // e
+console.log(userName[0].repeat(3).toLocaleLowerCase()); // eee
