e0e0f8ddd10e021893b02719c244036c3a267442
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 11:46:27 +0200

    Solve the fourth challenge from Arrays And Methods

---
+++ main.js
@@ -0,0 +1,7 @@
+let website = "Go";
+let numberTwo = website.length;
+let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
+
+console.log(
+  words[numberTwo][numberTwo - numberTwo].slice(numberTwo).toUpperCase()
+); // ZERO
