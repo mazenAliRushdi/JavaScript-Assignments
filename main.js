d1914b86bc613aca7f8b0ca0d29ecb11cabe1acd
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 13:17:47 +0200

    print friends not starting with 'A' using a while loop

---
+++ main.js
@@ -0,0 +1,14 @@
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+
+while (index < friends.length) {
+  if (
+    typeof friends[index] !== typeof counter &&
+    !friends[index].startsWith("A")
+  ) {
+    counter++;
+    console.log(`"${counter} => ${friends[index]}"`);
+  }
+  index++;
+}
