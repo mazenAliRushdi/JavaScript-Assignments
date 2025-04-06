671d8a8a5c96e85c6b5672352967971e470a57d5
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 12:46:36 +0200

    Print numbers with nested loop

---
+++ main.js
@@ -0,0 +1,10 @@
+let start = 1;
+let end = 6;
+let breaker = 2;
+
+for (let i = start; i <= end; i++) {
+  console.log(i);
+  for (let j = start; j <= breaker + breaker; j += breaker) {
+    console.log("-- " + j);
+  }
+}
