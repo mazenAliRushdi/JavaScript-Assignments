0e599ac215bce285a8b32d8fe52095656f5d9811
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 07:55:34 +0200

    Normal Concatenate and Template Literals Way

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-// The first way to cancel the code
+// Add Variables Here
+let numberOne = 10;
+let numberTwo = 20;
 
-// console.log("Iam In Console");
-// document.write("Iam In Page");
+// Ouput
+console.log(numberOne + "" + numberTwo); // Normal Concatenate => 1020
+console.log(typeof (numberOne + "" + numberTwo)); // Normal Concatenate => String
+console.log(`${numberOne}${numberTwo}`); // Template Literals Way => 1020
+console.log(typeof `${numberOne}${numberTwo}`); // Template Literals Way => String
 
-// The Second way to cancel the code
-
+console.log(numberTwo + "\n" + numberOne);
 /*
-    console.log("Iam In Console");
-    document.write("Iam In Page");
+  Normal Concatenate
+  20
+  10
+*/
+
+console.log(`${numberTwo}\n${numberOne}`);
+/*
+  Template Literals Way
+  20
+  10
 */
