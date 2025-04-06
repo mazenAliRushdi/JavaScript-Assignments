f7dc0bbe6c7787011ad890b21d3c77f87b7c20a3
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 11:26:12 +0200

    Remove first and last elements of array using array methods

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
-let num = 3;
+let friends = ["Ahmed", "Eman", "Osama", "Gamal"];
 
-// Method 1
-myFriends.length = num;
-console.log(myFriends); // ["Ahmed", "Elham", "Osama"]
+// Write Your Code Here
+friends.shift();
+friends.pop();
 
-// Reset myFriends to its original value for the second method
-myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
-
-// Method 2
-let end = myFriends.indexOf("Gamal");
-let newArray = myFriends.slice(0, end);
-console.log(newArray); // ["Ahmed", "Elham", "Osama"]
+console.log(friends); // ["Eman", "Osama"]
