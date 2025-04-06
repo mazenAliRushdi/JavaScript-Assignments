4cbc356fb74bca43b9e460f1aa728c92e7f9213c
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 10:16:22 +0200

    Replace question marks with appropriate operators for true boolean results

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log(100 == "100");
+console.log(100 < 1000);
+console.log(110 > 100 && 10 < 20);
+console.log(-10 == "-10");
+console.log(!false && -50 < -40);
+console.log(!true || 10 < -"-40");
+console.log(+"10" === 10);
+console.log(20 != false);
