9ec43a16b98b3b6882e7c0da8b6387e8538dfe18
mazen <mazen.ali.rushdy@gmail.com>
2025-04-06 09:11:22 +0200

    Extract and format numeric values from string variable

---
+++ main.js
@@ -0,0 +1,7 @@
+let myVar = "100.56789 Views";
+
+console.log(parseInt(myVar));
+console.log(typeof parseInt(myVar));
+
+console.log(parseFloat(myVar).toFixed(2));
+console.log(typeof parseFloat(myVar).toFixed(2));
